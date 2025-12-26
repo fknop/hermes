45d4abd005932043f97e07eb49fae28ae7fd94a2
Florian Knop <6775689+fknop@users.noreply.github.com>
2025-12-26 14:50:16 +0100

    tweak parse_duration function

--- hermes_cli/src/parsers.rs
+++ hermes_cli/src/parsers.rs
@@ -5,8 +5,16 @@ pub fn parse_duration(input: &str) -> Result<jiff::SignedDuration, String> {
         return Ok(duration);
     }
 
-    input
+    if let Ok(duration) = input
         .parse::<jiff::Span>()
         .and_then(|span| span.to_duration(SpanRelativeTo::days_are_24_hours()))
-        .map_err(|e| format!("invalid duration {e}"))
+    {
+        return Ok(duration);
+    }
+
+    if let Ok(seconds) = input.parse::<i64>() {
+        return Ok(jiff::SignedDuration::from_secs(seconds.abs()));
+    }
+
+    Err(String::from("Invalid duration"))
 }
