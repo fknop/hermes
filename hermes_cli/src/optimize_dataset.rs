06219bcb0dcf7ff09ce619c99726e68298e34ca2
Florian Knop <6775689+fknop@users.noreply.github.com>
2025-12-27 10:05:18 +0100

    experiment with multi progress

--- hermes_cli/src/optimize_dataset.rs
+++ hermes_cli/src/optimize_dataset.rs
@@ -8,7 +8,7 @@ use hermes_optimizer::{
         solver_params::{SolverParams, Termination, Threads},
     },
 };
-use indicatif::{ProgressBar, ProgressStyle};
+use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
 use tracing::info;
 
 use crate::{file_utils::read_folder, parsers};
@@ -38,8 +38,23 @@ pub fn run(args: OptimizeDatasetArgs) -> Result<(), anyhow::Error> {
         read_folder(&args.dataset)?
     };
 
-    for path in paths {
-        let vrp = SolomonParser::from_file(&path).unwrap();
+    let multi_bar = MultiProgress::new();
+    let style = ProgressStyle::with_template("{prefix:.bold} [{elapsed_precise}] {msg}").unwrap();
+    let seconds = args.timeout.as_secs();
+
+    let mut bars: Vec<_> = paths
+        .iter()
+        .map(|path| {
+            let pb = multi_bar.add(ProgressBar::new(seconds as u64));
+            pb.set_style(style.clone());
+            pb.set_prefix(path.file_name().unwrap().to_string_lossy().into_owned());
+            pb.set_message("pending...");
+            pb
+        })
+        .collect();
+
+    for (i, path) in paths.iter().enumerate() {
+        let vrp = SolomonParser::from_file(path).unwrap();
 
         let solver = Solver::new(
             vrp,
@@ -50,34 +65,36 @@ pub fn run(args: OptimizeDatasetArgs) -> Result<(), anyhow::Error> {
             },
         );
 
-        let seconds = args.timeout.as_secs();
-        let bar = Arc::new(ProgressBar::new(seconds as u64));
-
-        bar.enable_steady_tick(Duration::from_secs(1));
+        let bar = &mut bars[i]; //Arc::new(ProgressBar::new(seconds as u64));
+        bar.set_message("running...");
+        bar.reset_elapsed();
+        bar.enable_steady_tick(Duration::from_millis(100));
 
         bar.set_style(
-            ProgressStyle::default_bar()
-                .template("[{bar:40}] ({elapsed}/{len}s)")
-                .unwrap(),
+            style.clone(), // ProgressStyle::default_bar()
+                           //     .template("({elapsed}/{len}s)")
+                           //     .unwrap(),
         );
-        let t_bar = Arc::clone(&bar);
-        let ticker = thread::spawn(move || {
-            for i in 0..seconds {
-                t_bar.set_position(i as u64);
-                thread::sleep(Duration::from_secs(1));
-            }
-        });
+        // let t_bar = Arc::clone(&bar);
+        // let ticker = thread::spawn(move || {
+        //     for i in 0..seconds {
+        //         t_bar.set_position(i as u64);
+        //         thread::sleep(Duration::from_secs(1));
+        //     }
+        // });
 
         solver.solve();
         let best_solution = solver.current_best_solution();
 
-        bar.finish_and_clear();
-        ticker.join().unwrap();
-
         if let Some(best_solution) = best_solution {
-            info!("Best solution found: {:?}", best_solution.score);
+            let n_routes = best_solution.solution.non_empty_routes_count();
+            let total_transport_cost = best_solution.solution.total_transport_costs();
+            bar.finish_with_message(format!(
+                "Finished - routes = {}, costs = {}",
+                n_routes, total_transport_cost
+            ));
         } else {
-            info!("No solution found");
+            bar.finish_with_message(format!("No solution"));
         }
     }
 
